009c19039ff6f186b8f75d167b7e3b96f28dbacf
Harold Guzman <Gzmn.h10195@gmail.com>
2024-05-02 17:20:28 +0800

    Initial commit

---
+++ src/script/apps.js
@@ -0,0 +1,97 @@
+// popup ads timer
+adspopup(5)
+
+// menu animation timer
+menuAnimations(1000, 400)
+
+// icon animation 
+animation_icon_timer(1000)
+
+// button animation
+button_animation_timer(1000)
+
+
+
+
+
+function adspopup(time) {
+    let timers = time * 1000
+    setTimeout(() => {
+        $(".popup").fadeIn()
+    
+        setTimeout(() => {
+            $(".popup").css({
+                "display":"flex"
+            })
+    
+            $("#ads").css({
+                "display":"block"
+            })
+        }, 100);
+    }, timers);
+    
+    
+    $(".popup").click(function(){
+        $(this).fadeOut()
+        $("#ads").fadeOut()
+    })
+}
+
+
+
+
+
+function menuAnimations(delay,animation_time) {
+let delay_time = delay
+let timer = animation_time
+
+setTimeout(() => {
+    for (let ani = 0; ani < $(".menuchoice").length; ani++) {
+
+        setTimeout(() => {
+            $(".menuchoice").eq(ani).css({
+                "top": "0"
+            });
+        }, ani * 100 + timer);
+      
+    }
+}, delay_time);
+}
+
+
+
+function animation_icon_timer(timer){
+        setTimeout(() => {
+            $("#icons").css({
+                "left": "105"
+            });
+        }, timer);
+    
+        setInterval(() => {
+            $("#icons").css({
+                "left": "-120"
+            });
+            setInterval(() => {
+                $("#icons").css({
+                    "left": "0"
+                });
+            }, 1160);
+        }, 1110);
+}
+
+
+function button_animation_timer(timer){
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth <= 796) {
+
+
+    }else{
+        setTimeout(() => {
+            $(".memberbtn").fadeIn()
+        }, timer);
+    }
+}
+
+
+
